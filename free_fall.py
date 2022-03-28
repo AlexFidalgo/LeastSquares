869fef9f99f0af7b7534def93d4699f6cb71bc66
Alex Fidalgo <67179630+AlexFidalgo@users.noreply.github.com>
2022-03-28 06:22:30 -0300

    Update and rename py.py to free_fall.py

--- free_fall.py
+++ free_fall.py
@@ -5,13 +5,13 @@ h = np.loadtxt("h_data.txt")
 t = np.loadtxt("t_data.txt")
 
 plt.scatter(t, h)
-plt.xlabel("tempo")
-plt.ylabel("altura")
+plt.xlabel("time")
+plt.ylabel("height")
 plt.show()
 
 At = np.asmatrix([np.ones(len(t)), t, t**2])
 A = np.transpose(At)
-G = np.matmul(At, A) #matriz do sistema normal
-d = np.transpose(np.matmul(At,h)) #lado direito do sistema normal
+G = np.matmul(At, A) #normal system matrix
+d = np.transpose(np.matmul(At,h)) #normal system rhs
 x = np.linalg.solve(G, d)
 
