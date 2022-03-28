6cbde0531cea280ac906d3ad12e1d2b60a9dcbf6
Alex Fidalgo <67179630+AlexFidalgo@users.noreply.github.com>
2022-03-28 06:51:59 -0300

    Update free_fall.py

--- free_fall.py
+++ free_fall.py
@@ -7,7 +7,7 @@ t = np.loadtxt("t_data.txt")
 plt.scatter(t, h)
 plt.xlabel("time")
 plt.ylabel("height")
-plt.show()
+#plt.show()
 
 # At * A * x = At * b => G * x = d
 At = np.asmatrix([np.ones(len(t)), t, t**2])
@@ -16,3 +16,8 @@ G = np.matmul(At, A) #normal system matrix
 d = np.transpose(np.matmul(At,h)) #normal system rhs
 x = np.linalg.solve(G, d)
 
+def y(t):
+    return float(x[0]) + float(x[1])*t + float(x[2])*(t**2)
+
+plt.plot(t, y(t), 'r')
+plt.show()
